55ddcdb9bb80056a44244dd3b358d22a0444ca6e
dofury <cha_dh1004@naver.com>
2023-02-02 21:41:59 +0900

    앱 하단 네비게이션 바 제작

--- app/src/main/java/com/dofury/moneycycle/MainActivity.kt
+++ app/src/main/java/com/dofury/moneycycle/MainActivity.kt
@@ -9,9 +9,15 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.dofury.moneycycle.databinding.ActivityMainBinding
 
+private const val TAG_HOME = "home_fragment"
+private const val TAG_SETTING = "setting_fragment"
+
 class MainActivity : AppCompatActivity() {
+
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -22,16 +28,56 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setFragment(TAG_HOME, HomeFragment())
+
+        binding.navigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.homeFragment -> setFragment(TAG_HOME, HomeFragment())
+                R.id.settingFragment -> setFragment(TAG_SETTING, SettingFragment())
+            }
+            true
+        }
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+/*        val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)*/
 
-        binding.fab.setOnClickListener { view ->
+/*        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }*/
+    }
+
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val fragTransaction = manager.beginTransaction()
+
+        if(manager.findFragmentByTag(tag)==null){
+            fragTransaction.add(R.id.nav_host_fragment_content_main,fragment,tag)
         }
+
+        val home = manager.findFragmentByTag(TAG_HOME)
+        val setting = manager.findFragmentByTag(TAG_SETTING)
+
+        if(home != null){
+            fragTransaction.hide(home)
+        }
+        if(setting != null){
+            fragTransaction.hide(setting)
+        }
+
+        if(tag == TAG_HOME){
+            if(home!=null){
+                fragTransaction.show(home)
+            }
+        }
+        else if(tag == TAG_SETTING){
+            if(setting!=null){
+                fragTransaction.show(setting)
+            }
+        }
+        fragTransaction.commitAllowingStateLoss()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
